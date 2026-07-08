0bb36bd0dd85c0651dd361c131f8dbc7a90c06bb
Ksk3110 <ksk3110saito@icloud.com>
2026-07-08 17:19:45 +0900

    commonalized & score visualization

--- 2d/manual.py
+++ 2d/manual.py
@@ -1,17 +1,13 @@
 import numpy as np
 from dolfinx import mesh, fem, io
 from mpi4py import MPI
+import sys
 import basix.ufl
 
 from lib import right_side_cp_to_whole, cp_to_density_light, solve_helmholtz
+from param import f, f_source, k_val, x_min, x_max, y_min, y_max, nx, ny
 
-# 基本パラメータ
-f = 300.0
-omega = 2 * np.pi * f
-c = 343.0 # 音速
-k_val = omega / c
-
-domain = mesh.create_rectangle(MPI.COMM_WORLD, points=[[0.0, 0.0], [100.0, 100.0]], n=[500, 500])
+domain = mesh.create_rectangle(MPI.COMM_WORLD, points=[[x_min, y_min], [x_max, y_max]], n=[nx, ny])
 
 sub_element = basix.ufl.element("Lagrange", domain.topology.cell_name(), 1)
 element = basix.ufl.blocked_element(sub_element, shape=(2,))
@@ -26,25 +22,33 @@ rho_function = fem.Function(V_rho)
 rho_function.name = "Density"
 
 right_side_cp = np.array([
-    [40.0, 20.0],
-    [40.0, 50.0],
-    [30.0, 50.0],
-    [40.0, 80.0],
+    [15.0, 2.0],
+    [15.0, 13.0],
+    [11.0, 14.0],
+    [6.0, 15.0],
 ])
 
-full_cp = right_side_cp_to_whole(right_side_cp, origin_coord=[50.0, 0.0])
+full_cp = right_side_cp_to_whole(right_side_cp, origin_coord=[(x_min+x_max)/2, y_min])
 
 rho_function.x.array[:] = cp_to_density_light(full_cp, V_rho, path_width=1)
 
 def open_boundaries(x):
-    return (np.isclose(x[0], 0.0) | np.isclose(x[0], 100.0) |
-            np.isclose(x[1], 0.0) | np.isclose(x[1], 100.0))
+    return (np.isclose(x[0], x_min) | np.isclose(x[0], x_max) |
+            np.isclose(x[1], y_min) | np.isclose(x[1], y_max))
 
 
 f_source = np.array([50.0, 50.0])
 u_sol = solve_helmholtz(V, rho_function, k_val, f_source, open_boundaries)
 u_sol.name = "Solution"
 
+V_score = fem.functionspace(domain, ("DG", 0))
+step_score = fem.Function(V_score, name="Step_Score")
+step_score.x.array[:] = 13.0
+
 xdmf.write_function(rho_function, 0.0)
 xdmf.write_function(u_sol, 0.0)
+xdmf.write_function(step_score, 0.0)
 xdmf.close()
+
+MPI.Finalize()
+sys.exit()
--- 2d/param.py
+++ 2d/param.py
@@ -11,7 +11,13 @@ k_val = omega / c
 x_min, x_max = 0.0, 100.0
 y_min, y_max = 0.0, 30.0
 
+res = 5 # 単位長さあたりのメッシュの解像度
+nx = int((x_max - x_min) * res)
+ny = int((y_max - y_min) * res)
+
 # 設計可能領域
 def desinable(coord):
-  return (35.0 <= coord[0] <= 55.0) and
-          (1.0 <= coord[1] <= 20.0)
+  return (
+      (35.0 <= coord[0] <= 55.0) and
+      (1.0 <= coord[1] <= 20.0)
+  )
