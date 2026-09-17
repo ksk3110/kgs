59f1cccb3bf6270f716a2b9a8f1dd68484d9674d
ksk3110 <ksk3110saito@icloud.com>
2026-09-17 18:20:48 +0900

    param.py -> params.py

--- 2d-alpha/params.py
+++ 2d-alpha/params.py
@@ -4,12 +4,7 @@ from dolfinx import mesh
 # ドメインメッシュを作成
 comm = MPI.COMM_WORLD # MPIコミュニケータ
 points = ((-50.0, 0.0), (50, 30)) # 領域の左下と右上の座標
-cell_type = dolfinx.mesh.CellType.triangle # 分割の方法
-domain: mesh.Mesh = mesh.create_rectangle(
-  comm,
-  points,
-  cell_type
-)
+cell_type = mesh.CellType.triangle # 分割の方法
 
 test_freqs = [125.0, 250.0, 500.0, 1000.0, 2000.0]
 test_amplitude = 80.0 # [dB]
