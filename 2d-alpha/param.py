c2dcdab9a7129fbdbd21a3e9222072c58504595b
Ksk3110 <ksk3110saito@icloud.com>
2026-09-17 12:44:20 +0900

    Refactor domain creation and add test parameters
    Refactor create_domain function and add test parameters.

--- 2d-alpha/param.py
+++ 2d-alpha/param.py
@@ -2,14 +2,14 @@ from mpi4py import MPI
 from dolfinx import mesh
 
 # ドメインメッシュを作成
-def create_domain() -> mesh.Mesh:
-  comm = MPI.COMM_WORLD # MPIコミュニケータ
-  points = ((-50.0, 0.0), (50, 30)) # 領域の左下と右上の座標
-  cell_type = dolfinx.mesh.CellType.triangle # 分割の方法
-  domain = mesh.create_rectangle(
-    comm,
-    points,
-    cell_type
-  )
+comm = MPI.COMM_WORLD # MPIコミュニケータ
+points = ((-50.0, 0.0), (50, 30)) # 領域の左下と右上の座標
+cell_type = dolfinx.mesh.CellType.triangle # 分割の方法
+domain: mesh.Mesh = mesh.create_rectangle(
+  comm,
+  points,
+  cell_type
+)
 
-  return domain
+test_freqs = [125.0, 250.0, 500.0, 1000.0, 2000.0]
+test_amplitude = 80.0 # [dB]
